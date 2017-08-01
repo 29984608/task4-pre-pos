2fa780afa80bf52a24f074bdd6a67aaff1edee64
29984608 <29984608@qq.com>
2017-08-01 16:30:07 +0800

    完成11道题

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,12 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  let sameElements = [];
+  let collectionB = object_b.value;
+
+  collection_a.map(element => {
+    if (collectionB.includes(element)) sameElements.push(element);
+  });
+
+  return sameElements;
 }
 
 module.exports = collect_same_elements;
--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,12 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  let sameElements = [];
+  let collectionB = object_b.value;
+
+  collection_a.map(obj => {
+    if (collectionB.includes(obj.key)) sameElements.push(obj.key);
+  });
+
+  return sameElements;
 }
 
 module.exports = collect_same_elements;
