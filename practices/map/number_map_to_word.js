4f1cfb2ee03d39333c6cfaec2bc95321458955b6
29984608 <29984608@qq.com>
2017-08-01 17:55:57 +0800

    let letters = getRoundsLetters(2);

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,15 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  let letters = getBasicLetter();
+
+  return collection.map(element => element = letters[element - 1]);
+};
+function getBasicLetter() {
+  let basicLetter = [];
+  for (let i = 97; i < 97 + 26; ++i)
+    basicLetter.push(String.fromCharCode(i));
+
+  return basicLetter;
 };
 
 module.exports = number_map_to_word;
