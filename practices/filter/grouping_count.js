968ad128da15e7fec8569377fec0ea61dab0627a
29984608 <29984608@qq.com>
2017-08-01 17:16:37 +0800

    筛选器完成

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -2,7 +2,16 @@
 
 function grouping_count(collection) {
 
-  //在这里写入代码
+  let groupingCount = {};
+
+  collection.map(element => {
+    if (groupingCount[element] !== undefined)
+      ++groupingCount[element];
+    else
+      groupingCount[element] = 1;
+  });
+
+  return groupingCount;
 }
 
 module.exports = grouping_count;
--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+  return collection_a.filter(element => canBeDivisible(collection_b, element))
+}
+function canBeDivisible(collection, element) {
+  return collection.some(temp => element % temp === 0);
 }
 
 module.exports = choose_divisible_integer;
